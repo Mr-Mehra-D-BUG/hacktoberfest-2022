f558ce9d1ee9587ee36f5663b7fac411ddd33b87
Devesh Mehara <96515074+MehraDevesh2022@users.noreply.github.com>
2022-10-03 03:04:10 +0530

    Create hello_world_MehraDevesh2022.java

---
+++ scripts/hello_world_MehraDevesh2022.java
@@ -0,0 +1,10 @@
+LANGUAGE: JAVA
+  AUTHOR: Devesh mehra
+  GITHUB: https://github.com/MehraDevesh2022
+*/
+
+public class HelloWorldWithJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
